12ea998cbadf45994b898a66e3cfd19a302de0f8
Davy Landman <davy.landman@gmail.com>
2013-05-20 15:58:43 +0200

    Matched github style guideline

--- main.rb
+++ main.rb
@@ -1,24 +1,24 @@
 require 'set'
 
-imgBaseDir = "/Users/davy/Pictures/"
-targetDir = "/Users/davy/Dropbox/Pictures Backup/"
+img_base_dir = "/Users/davy/Pictures/"
+target_dir = "/Users/davy/Dropbox/Pictures Backup/"
 
-puts "Checking input directory #{imgBaseDir} for images to backup"
-allImages = Dir.glob("#{imgBaseDir}**/*.{jpg,jpeg,png,tiff,tif,psd}", File::FNM_CASEFOLD)
-puts "Got #{allImages.size} amount of images to progress"
+puts "Checking input directory #{img_base_dir} for images to backup"
+all_images = Dir.glob("#{img_base_dir}**/*.{jpg,jpeg,png,tiff,tif,psd}", File::FNM_CASEFOLD)
+puts "Got #{all_images.size} amount of images to progress"
 
 Action = Struct.new(:original, :target)
 
-backupActions = Set.new
+backup_actions = Set.new
 
-allImages.each do |currentImage|
-  newFileName = currentImage.gsub(imgBaseDir, targetDir)
+all_images.each do |current_image|
+  newFileName = current_image.gsub(img_base_dir, target_dir)
   newFileName += ".jpg" if (File.extname(newFileName).downcase != ".jpg")
   if !(File.exists?(newFileName)) && !(File.exists?(newFileName.gsub(".jpg", "-0.jpg")))
     ac = Action.new
-    ac.original = currentImage
+    ac.original = current_image
     ac.target = newFileName
-    backupActions << ac
+    backup_actions << ac
   end
 end
-puts "#{backupActions.size} to actually backup"
+puts "#{backup_actions.size} to actually backup"
