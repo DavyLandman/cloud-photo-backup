99da5cb5845151a9e53ec329a8198f651b800253
Davy Landman <davy.landman@gmail.com>
2013-05-20 16:18:52 +0200

    Splitting up the code in separate methods

--- main.rb
+++ main.rb
@@ -1,24 +1,40 @@
 require 'set'
 
+
+
+class BackupJob
+  def initialize(original, target)
+    @original = original
+    @target = target
+  end
+end
+
+def getImages(img_base_dir)
+  puts "Checking input directory #{img_base_dir} for images to backup"
+  all_images = Dir.glob("#{img_base_dir}**/*.{jpg,jpeg,png,tiff,tif,psd}", File::FNM_CASEFOLD)
+  puts "Got #{all_images.size} amount of images to progress"
+  return all_images
+end
+
+def createJobs(all, img_base_dir, target_dir)
+  backup_actions = Set.new
+
+  all.each do |current_image|
+    target_filename = current_image.gsub(img_base_dir, target_dir)
+    target_filename += ".jpg"  unless /\.(jpg|jpeg)/.match(File.extname(target_filename).downcase)
+       
+    unless File.exists?(target_filename) || File.exists?(target_filename.gsub(".jpg", "-0.jpg"))
+      backup_actions << BackupJob.new(current_image, target_filename) 
+    end
+  end
+
+  return backup_actions
+end
+
 img_base_dir = "/Users/davy/Pictures/"
 target_dir = "/Users/davy/Dropbox/Pictures Backup/"
 
-puts "Checking input directory #{img_base_dir} for images to backup"
-all_images = Dir.glob("#{img_base_dir}**/*.{jpg,jpeg,png,tiff,tif,psd}", File::FNM_CASEFOLD)
-puts "Got #{all_images.size} amount of images to progress"
+all = getImages(img_base_dir)
+backup_actions = createJobs(all, img_base_dir, target_dir)
 
-Action = Struct.new(:original, :target)
-
-backup_actions = Set.new
-
-all_images.each do |current_image|
-  newFileName = current_image.gsub(img_base_dir, target_dir)
-  newFileName += ".jpg" if (File.extname(newFileName).downcase != ".jpg")
-  if !(File.exists?(newFileName)) && !(File.exists?(newFileName.gsub(".jpg", "-0.jpg")))
-    ac = Action.new
-    ac.original = current_image
-    ac.target = newFileName
-    backup_actions << ac
-  end
-end
 puts "#{backup_actions.size} to actually backup"
