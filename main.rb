7e224f480ed2d83e5b554b4167321d6952a2004b
Davy Landman <davy.landman@gmail.com>
2013-05-20 15:51:58 +0200

    Switched to spaces

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require 'set'
 
-
 imgBaseDir = "/Users/davy/Pictures/"
 targetDir = "/Users/davy/Dropbox/Pictures Backup/"
 
+puts "Checking input directory #{imgBaseDir} for images to backup"
 allImages = Dir.glob("#{imgBaseDir}**/*.{jpg,jpeg,png,tiff,tif,psd}", File::FNM_CASEFOLD)
 puts "Got #{allImages.size} amount of images to progress"
 
@@ -12,13 +12,13 @@ Action = Struct.new(:original, :target)
 backupActions = Set.new
 
 allImages.each do |currentImage|
-	newFileName = currentImage.gsub(imgBaseDir, targetDir)
-	newFileName += ".jpg" if (File.extname(newFileName).downcase != ".jpg")
-	if !(File.exists?(newFileName)) && !(File.exists?(newFileName.gsub(".jpg", "-0.jpg")))
-		ac = Action.new
-		ac.original = currentImage
-		ac.target = newFileName
-		backupActions << ac
-	end
+  newFileName = currentImage.gsub(imgBaseDir, targetDir)
+  newFileName += ".jpg" if (File.extname(newFileName).downcase != ".jpg")
+  if !(File.exists?(newFileName)) && !(File.exists?(newFileName.gsub(".jpg", "-0.jpg")))
+    ac = Action.new
+    ac.original = currentImage
+    ac.target = newFileName
+    backupActions << ac
+  end
 end
 puts "#{backupActions.size} to actually backup"
